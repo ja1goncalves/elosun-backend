77f854d3c59d5e5c89636407500a6276f1fa2117
João Paulo F. Gonçalves da Silva <joaosilva@mangue3.com>
2019-12-11 13:08:24 -0300

    Admin with Stock

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,8 +37,6 @@ class RouteServiceProvider extends ServiceProvider
     {
         $this->mapApiRoutes();
 
-        $this->mapWebRoutes();
-
         $this->mapAuthRoutes();
 
         $this->mapAdminRoutes();
@@ -48,19 +46,6 @@ class RouteServiceProvider extends ServiceProvider
         //
     }
 
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapWebRoutes()
-    {
-        Route::middleware('web')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
-    }
 
     /**
      * Define the "api" routes for the application.
--- routes/admin.php
+++ routes/admin.php
@@ -6,6 +6,22 @@ Route::group(['prefix' => 'users', 'middleware' => ['auth:api']], function () {
     Route::resource('/', 'UsersController', ['except' => ['create', 'delete']]);
 });
 
+Route::group(['prefix' => 'clients', 'middleware' => ['auth:api']], function () {
+    Route::resource('/', 'ClientsController', ['except' => ['create', 'delete']]);
+});
+
+Route::group(['prefix' => 'providers', 'middleware' => ['auth:api']], function () {
+    Route::resource('/', 'ProvidersController', ['except' => ['create', 'delete']]);
+});
+
+Route::group(['prefix' => 'orders', 'middleware' => ['auth:api']], function () {
+    Route::resource('/', 'OrdersController', ['except' => ['create', 'delete']]);
+});
+
 Route::group(['prefix' => 'distributors', 'middleware' => ['auth:api']], function () {
     Route::resource('/', 'EnergyDistributorsController', ['except' => ['create', 'delete']]);
+});
+
+Route::group(['prefix' => 'stock', 'middleware' => ['auth:api']], function () {
+    Route::resource('/', 'Stock\StockController', ['except' => ['create', 'delete']]);
 });
