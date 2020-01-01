d2775a2b57dafbb23df7ca506485d144a76da4fc
Joao Paulo Felix <joaosilva@mangue3.com>
2020-01-01 13:59:59 -0300

    EnergyStations | Seed | modification csv

--- database/seeds/ElectricStationsSeeder.php
+++ database/seeds/ElectricStationsSeeder.php
@@ -18,40 +18,45 @@ class ElectricStationsSeeder extends Seeder
         foreach ($file as $item) {
             $csv = str_getcsv($item, ';');
 
+            $energy_distributor = \App\Entities\EnergyDistributor::query()
+                ->select(['id'])
+                ->where('initials', '=', $csv[1])
+                ->first()->toArray();
+
             $production_type = \App\Entities\ProductionTypes::query()
                 ->select(['id'])
-                ->where('class', '=', $csv[2])
+                ->where('class', '=', $csv[3])
                 ->first()->toArray();
 
             $modality = \App\Entities\Modalities::query()
                 ->select(['id'])
-                ->where('description', '=', $csv[4])
+                ->where('description', '=', $csv[5])
                 ->first()->toArray();
 
             $address = [
-                'city' => $csv[6],
-                'state' => $csv[7],
-                'zip_code' => preg_replace('/\D/', '', $csv[8])
+                'city' => $csv[7],
+                'state' => $csv[8],
+                'zip_code' => preg_replace('/\D/', '', $csv[9])
             ];
             $address = \App\Entities\Address::query()->updateOrCreate($address, $address)->toArray();
 
             $station = [
                 'name' => '',
                 'code_gd' => $csv[0],
-                'holder' => $csv[1],
+                'holder' => $csv[2],
                 'production_type_id' => $production_type['id'],
-                'subgroup' => $csv[3],
+                'subgroup' => $csv[4],
                 'area' => '',
-                'potency_kW' => (float)str_replace(',', '.', str_replace("\\", '', $csv[12])),
+                'potency_kW' => (float)str_replace(',', '.', str_replace("\\", '', $csv[13])),
                 'inverters' => '',
                 'modules' => '',
-                'total_ucs' => $csv[5],
+                'total_ucs' => $csv[6],
                 'modality_id' => $modality['id'],
                 'address_id' => $address['id'],
-                'energy_distributors_id' => '',
+                'energy_distributor_id' => $energy_distributor['id'],
                 'created_at' => \Carbon\Carbon::now(),
                 'updated_at' => \Carbon\Carbon::now(),
-                'connection_at' => \Carbon\Carbon::createFromFormat('d/m/Y', $csv[9])->format('Y-m-d hh:mm:ss')
+                'connection_at' => \Carbon\Carbon::createFromFormat('d/m/Y', $csv[10])->format('Y-m-d hh:mm:ss')
             ];
 
             DB::table('electric_stations')->insert($station);
