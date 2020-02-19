c916d985154aed6851e1b415160d8af37e6f7916
ThiagoDes <thiagothic25@gmail.com>
2020-02-19 09:03:10 -0300

    Atualização #2  Route admin

--- routes/admin.php
+++ routes/admin.php
@@ -7,14 +7,14 @@ Route::group(['prefix' => 'users', 'middleware' => ['auth:api']], function () {
     Route::post('/search', 'UsersController@searchs');
 });
 
-Route::group(['prefix' => 'clients', 'middleware' => []], function () {
+Route::group(['prefix' => 'clients', 'middleware' => ['auth:api']], function () {
     Route::resource('/', 'ClientsController', ['except' => ['create', 'delete']]);
     Route::get('/lead', 'ClientsController@listLead');
     Route::post('/search', 'ClientsController@searchs');
     Route::post('/lead/search', 'ClientsController@searchs');
 });
 
-Route::group(['prefix' => 'providers', 'middleware' => []], function () {
+Route::group(['prefix' => 'providers', 'middleware' => ['auth:api']], function () {
     Route::resource('/', 'ProvidersController', ['except' => ['create', 'delete']]);
     Route::get('/lead', 'ProvidersController@listLead');
     Route::post('/search', 'ProvidersController@searchs');
@@ -25,14 +25,14 @@ Route::group(['prefix' => 'orders', 'middleware' => ['auth:api']], function () {
     Route::resource('/', 'OrdersController', ['except' => ['create', 'delete']]);
 });
 
-Route::group(['prefix' => 'distributors', 'middleware' => []], function () {
+Route::group(['prefix' => 'distributors', 'middleware' => ['auth:api']], function () {
     Route::resource('/', 'EnergyDistributorsController', ['except' => ['create', 'delete']]);
     Route::get('/{id}', 'EnergyDistributorsController@show');
     Route::post('/update-list', 'EnergyDistributorsController@updateCrw');
     Route::post('/search', 'EnergyDistributorsController@searchs');
 });
 
-Route::group(['prefix' => 'stations', 'middleware' => []], function () {
+Route::group(['prefix' => 'stations', 'middleware' => ['auth:api']], function () {
     Route::resource('/', 'ElectricStationsController', ['except' => ['create', 'delete']]);
     Route::get('/{id}', 'ElectricStationsController@show');
     Route::post('/search', 'ElectricStationsController@searchs');
